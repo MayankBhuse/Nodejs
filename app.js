aa137663194ad85954620d9cac09e36406751dcb
Mayank Bhuse <bhusemayankmahendra@gmail.com>
2024-02-16 23:40:47 +0530

    init

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/', (req, res) => {
+  res.send('Kya haal hai bhai,yoo')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
